430b7f3a17b1706efdbb4e0b9085170c66ae2a31
kangCoder <khsofficial1213@gmail.com>
2022-03-27 21:41:36 +0900

    img upload complete

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,33 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const multer = require("multer");
+const fs = require("fs");
 
+const app = express();
+
+fs.readdir("uploads", (err) => {
+  if (err) {
+    fs.mkdirSync("uploads");
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).single("img");
+app.post("/api/upload", upload, (req, res, next) => {
+  res.status(201).send({
+    result: true,
+    fileInfo: req.file,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("success");
+  console.log(`Server is listening at PORT=${PORT}`);
 });
